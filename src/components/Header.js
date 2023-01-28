76bb13571fd0a3594c2c92994c0a586ab3454d6a
MahmudShakir <shakir.wt@gmail.com>
2023-01-28 23:31:06 +0600

    semantic tag added

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../icons_assets/Logo.svg'
+
+const Header = () => {
+  return (
+    <div>
+      <img src={Logo} alt='Logo'/>
+    </div>
+  )
+}
+
+export default Header
